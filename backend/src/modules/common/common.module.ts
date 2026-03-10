e9db15291a918163feb9967844c3710ae3087a5a
diamondjdev <cameronginther@gmail.com>
2026-03-10 08:49:07 -0500

    update: Remove CacheModule import from CommonModule

--- backend/src/modules/common/common.module.ts
+++ backend/src/modules/common/common.module.ts
@@ -3,10 +3,9 @@ import { HealthController } from './health/controller/health.controller';
 import { DbModule } from '../db/db.module';
 import { LoggerService } from './logging/services/logger.service';
 import { LoggingInterceptor } from './logging/interceptors/logging.interceptor';
-import { CacheModule } from '../cache/cache.module';
 
 @Module({
-    imports: [DbModule, CacheModule],
+    imports: [DbModule],
     controllers: [HealthController],
     providers: [LoggerService, LoggingInterceptor],
     exports: [LoggerService, LoggingInterceptor],
